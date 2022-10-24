baf0e216c2c9f69f7ec49518e6f2d732936591e3
José Ignacio <joseignacioig9910@gmail.com>
2022-10-24 11:28:32 +0200

    MVP Pokemon details

--- routes/app.routes.js
+++ routes/app.routes.js
@@ -17,6 +17,29 @@ router.get("/pokedex", async (req, res, next) => {
         }
 })
 
+router.get("/pokedex/:pokeIndex", async(req, res, next) => {
+    let {pokeIndex} = req.params
+    try {
+        const pokemonDetails = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeIndex}`)
+        const pokemonSpecies = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${pokeIndex}`)
+        const pokemonChain = await axios.get(`https://pokeapi.co/api/v2/evolution-chain/`)
+        const pokemonMinus = await axios.get(`https://pokeapi.co/api/v2/pokemon/${parseFloat(pokeIndex) - 1}`)
+        const pokemonContinue = await axios.get(`https://pokeapi.co/api/v2/pokemon/${parseFloat(pokeIndex) + 1}`)
+
+        
+        res.render("app/poke-details.hbs", {
+            pokemonDetails,
+            pokemonSpecies,
+            pokemonChain,
+            pokemonMinus,
+            pokemonContinue
+        })
+        console.log(pokemonContinue.data.id)
+    } catch (error) {
+        next(error)
+    }
+})
+
 
 
 module.exports = router;
